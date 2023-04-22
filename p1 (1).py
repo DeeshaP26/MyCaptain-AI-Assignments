40c135f63d4224a99797d8d5f98f773d9aa96459
DeeshaP26 <115894200+DeeshaP26@users.noreply.github.com>
2023-04-23 01:00:15 +0530

    Add files via upload

---
+++ p1 (1).py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Displaying "hello world !" on the screen
+#print is a python defined defined function 
+print("Hello World!")
+
+
+# In[ ]:
+
+
+
+
